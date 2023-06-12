38b4eb015d6039dc3eb1a20cf30a8c1103c17e74
WakilMahmud <wakilmahmud30@gmail.com>
2023-06-13 00:30:24 +0600

    Error Page is added

--- src/pages/Shared/ErrorPage/ErrorPage.jsx
+++ src/pages/Shared/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,21 @@
+import { Link, useRouteError } from "react-router-dom";
+import Lottie from "lottie-react";
+import ErrorPic from "../../../assets/Error.json";
+
 const ErrorPage = () => {
-	return <div>Error Page</div>;
+	useRouteError();
+	return (
+		<section className="flex items-center h-screen">
+			<div className="container flex flex-col items-center justify-center space-y-4">
+				<Lottie animationData={ErrorPic} loop={true} />
+				<div className="text-center">
+					<Link to="/">
+						<button className="btn btn-outline btn-info">Back to Home</button>
+					</Link>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default ErrorPage;
