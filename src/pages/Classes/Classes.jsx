6923f8426fea16286b39a9292b6f5dfd5822f669
WakilMahmud <wakilmahmud30@gmail.com>
2023-06-28 01:31:20 +0600

    Select Classes from Classes Page

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -2,12 +2,13 @@ import axios from "axios";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import useRole from "../../hooks/useRole";
-import Swal from "sweetalert2";
+import SelectAClass from "./SelectAClass";
 
 const Classes = () => {
 	const [approvedClasses, setApprovedClasses] = useState([]);
 	const { user } = useContext(AuthContext);
 	const [role] = useRole(user);
+
 	axios
 		.get("http://localhost:5000/approved-classes")
 		.then((res) => {
@@ -18,34 +19,11 @@ const Classes = () => {
 			console.error(error);
 		});
 
-	const handleSelect = () => {
-		if (!user) {
-			Swal.fire({
-				title: "Please Login before selecting the course",
-			});
-		}
-	};
 	return (
 		<div className="grid grid-cols-3 gap-2">
-			{approvedClasses.map((Class) => {
-				// console.log(Class?.availableSeats);
-				return (
-					<div key={Class?._id} className={`card card-compact w-96  shadow-xl ${Class?.availableSeats == 0 ? "bg-red-500" : "bg-base-100"}`}>
-						<figure>
-							<img className="h-64 w-full" src={Class?.classImage} alt="Class" />
-						</figure>
-						<div className="card-body">
-							<h2 className="card-title">{Class?.className}</h2>
-							<p>Instructor Name: {Class?.instructorName}</p>
-							<p>Available Seats: {Class?.availableSeats}</p>
-							<p>Price: {Class?.price}$</p>
-							<button onClick={() => handleSelect()} className="btn btn-info" disabled={Class?.availableSeats == 0 || role != "student"}>
-								Select
-							</button>
-						</div>
-					</div>
-				);
-			})}
+			{approvedClasses.map((Class) => (
+				<SelectAClass key={Class?._id} Class={Class} role={role} user={user}></SelectAClass>
+			))}
 		</div>
 	);
 };
---
+++ src/pages/Classes/SelectAClass.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import Swal from "sweetalert2";
+
+const SelectAClass = ({ Class, role, user }) => {
+	const [isDisabled, setIsDisabled] = useState(false);
+	const handleSelect = (Class) => {
+		if (!user) {
+			Swal.fire({
+				title: "Please Login before selecting the course",
+			});
+			return;
+		}
+		const saveClass = {
+			availableSeats: Class?.availableSeats,
+			classImage: Class?.classImage,
+			className: Class?.className,
+			instructorEmail: Class?.instructorEmail,
+			instructorName: Class?.instructorName,
+			price: Class?.price,
+			status: "booked",
+		};
+
+		fetch("http://localhost:5000/register-classes", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify(saveClass),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.insertedId) {
+					setIsDisabled(true);
+					Swal.fire({
+						icon: "success",
+						title: "Class is Booked",
+						showConfirmButton: false,
+						timer: 1500,
+					});
+				}
+			});
+	};
+	return (
+		<div key={Class?._id} className={`card card-compact w-96  shadow-xl ${Class?.availableSeats == 0 ? "bg-red-500" : "bg-base-100"}`}>
+			<figure>
+				<img className="h-64 w-full" src={Class?.classImage} alt="Class" />
+			</figure>
+			<div className="card-body">
+				<h2 className="card-title">{Class?.className}</h2>
+				<p>Instructor Name: {Class?.instructorName}</p>
+				<p>Available Seats: {Class?.availableSeats}</p>
+				<p>Price: {Class?.price}$</p>
+				<button onClick={() => handleSelect(Class)} className="btn btn-info" disabled={Class?.availableSeats == 0 || role != "student" || isDisabled}>
+					Select
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default SelectAClass;
