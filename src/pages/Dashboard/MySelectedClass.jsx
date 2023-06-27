4e6ccb647e7c7377c997e4ec4c9a688dd2d5e42e
WakilMahmud <wakilmahmud30@gmail.com>
2023-06-28 02:01:35 +0600

    My Selected Classes, Also Delete Operation is done

---
+++ src/pages/Dashboard/MySelectedClass.jsx
@@ -0,0 +1,39 @@
+import Swal from "sweetalert2";
+
+const MySelectedClass = ({ Class, refetch }) => {
+	const handleDelete = (id) => {
+		// console.log(id);
+		fetch(`http://localhost:5000/register-classes/${id}`, { method: "DELETE", headers: { "Content-Type": "application/json" } })
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.deletedCount > 0) {
+					refetch();
+					Swal.fire({
+						icon: "success",
+						title: `Class is deleted successfully`,
+						showConfirmButton: false,
+						timer: 1500,
+					});
+				}
+			});
+	};
+	return (
+		<div key={Class?._id} className="card card-compact w-96  shadow-xl bg-base-100">
+			<figure>
+				<img className="h-64 w-full" src={Class?.classImage} alt="Class" />
+			</figure>
+			<div className="card-body">
+				<h2 className="card-title">{Class?.className}</h2>
+				<p>Instructor Name: {Class?.instructorName}</p>
+				<p>Available Seats: {Class?.availableSeats}</p>
+				<p>Price: {Class?.price}$</p>
+				<button className="btn btn-error" onClick={() => handleDelete(Class?._id)}>
+					Delete
+				</button>
+				<button className="btn btn-info">Pay</button>
+			</div>
+		</div>
+	);
+};
+
+export default MySelectedClass;
--- src/pages/Dashboard/MySelectedClasses.jsx
+++ src/pages/Dashboard/MySelectedClasses.jsx
@@ -1,5 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import MySelectedClass from "./MySelectedClass";
+
 const MySelectedClasses = () => {
-	return <div>MySelectedClasses</div>;
+	const { data: selectedClasses = [], refetch } = useQuery({
+		queryKey: ["selectedClasses"],
+		queryFn: async () => {
+			const res = await fetch("http://localhost:5000/register-classes");
+			return res.json();
+		},
+	});
+	console.log(selectedClasses);
+	return (
+		<div className="grid grid-cols-2 gap-2">
+			{selectedClasses.map((Class) => (
+				<MySelectedClass key={Class?._id} Class={Class} refetch={refetch}></MySelectedClass>
+			))}
+		</div>
+	);
 };
 
 export default MySelectedClasses;
