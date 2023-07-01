2e8f6ceaca622e822c90dd2692de9f93263cf4bd
WakilMahmud <wakilmahmud30@gmail.com>
2023-07-01 22:03:01 +0600

    FAQ section is added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import FAQ from "../FAQ/FAQ";
 import PopularClasses from "../PopularClasses/PopularClasses";
 import PopularInstructors from "../PopularInstructors/PopularInstructors";
 import Slider from "../Slider/Slider";
@@ -8,6 +9,7 @@ const Home = () => {
 			<Slider></Slider>
 			<PopularClasses></PopularClasses>
 			<PopularInstructors></PopularInstructors>
+			<FAQ></FAQ>
 		</>
 	);
 };
